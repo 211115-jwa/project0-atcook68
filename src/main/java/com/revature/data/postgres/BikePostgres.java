7add308df2cd304a2a592d709ac6363ce9fd873f
Andy <56707541+atcook68@users.noreply.github.com>
2021-12-16 23:53:46 -0800

    Working on the BikePostgres.

--- src/main/java/com/revature/data/postgres/BikePostgres.java
+++ src/main/java/com/revature/data/postgres/BikePostgres.java
@@ -5,6 +5,8 @@ import com.revature.data.BikeDAO;
 import com.revature.util.ConnectionUtil;
 import java.util.List;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -13,6 +15,33 @@ public class BikePostgres implements BikeDAO {
 	private ConnectionUtil connUtil = ConnectionUtil.getConnectionUtil();
 			
 			
+	
+	public int create(Bike dataToAdd) {
+		int createId = 0;
+		try (Connection conn = connUtil.getConnection()){
+			conn.setAutoCommit(false);
+				String sql = "Insert into bike (id, bike_year, bike_brand, bike_color)"
+						+ "values (default, ?,?,?,?)";
+				
+				String[] keys = {"id"};
+				PreparedStatement prepStmt = conn.prepareStatement(sql, keys);
+				prepStmt.setString(1, dataToAdd.getBikeBrand());
+				prepStmt.setString(2, dataToAdd.getBikeColor());
+				prepStmt.setString(3, dataToAdd.getBikeYear());
+				
+				prepStmt.executeUpdate();
+				ResultSet resultSet = prepStmt.getGeneratedKeys();
+				if (resultSet.next()) {
+					createId = resultSet.getInt("id");
+					conn.commit();  // commits the database changes
+				}else {
+					conn.rollback();   // or not
+				}
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+		return createId;
+	}
 }
 
 
